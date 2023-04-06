9e011859a7311add2f7a00dc26d0f5280b824c70
auth <65650599+Authorises@users.noreply.github.com>
2023-04-06 22:20:04 +0100

    fix: made message history stored in EvictingQueue

--- src/main/java/dev/authorises/Main.java
+++ src/main/java/dev/authorises/Main.java
@@ -1,10 +1,15 @@
 package dev.authorises;
 
+import com.google.gson.Gson;
+import dev.authorises.messages.Message;
+import dev.authorises.mongodb.MongoDBConnector;
+import dev.authorises.services.MessageService;
 import io.javalin.Javalin;
 import io.javalin.websocket.WsContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,8 +20,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
+        Javalin.create()
+                .post("/message", ctx -> {
+                    Message message = new Message(UUID.fromString(ctx.queryParam("sender")), ctx.queryParam("message"), Long.valueOf(ctx.queryParam("time"))) ;
+                    MessageService.getInstance().processMessage(message);
+                })
+                .get("/messages", ctx -> {
+                    ctx.result(new Gson().toJson(MessageService.getInstance().getLastMessages()));
+                })
+                .start(1234);
 
     }
 }
--- src/main/java/dev/authorises/services/MessageService.java
+++ src/main/java/dev/authorises/services/MessageService.java
@@ -1,10 +1,15 @@
 package dev.authorises.services;
 
+import com.google.common.collect.EvictingQueue;
+import com.google.gson.Gson;
+import dev.authorises.Main;
 import dev.authorises.messages.Message;
 import dev.authorises.mongodb.MongoDBConnector;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -12,9 +17,11 @@ public class MessageService {
 
     private static MessageService instance;
     private List<Document> insertQueue;
+    private EvictingQueue<Message> messagesHistory;
 
     private MessageService(){
-        insertQueue = new ArrayList<>();
+        this.insertQueue = new ArrayList<>();
+        this.messagesHistory = EvictingQueue.create(500);
     }
 
     public static MessageService getInstance(){
@@ -24,16 +31,32 @@ public class MessageService {
         return instance;
     }
 
+
+    /**
+     * @return The last 500 messages, or if there are not 500 messages stored, load all messages stored.
+     */
+    public List<Message> getLastMessages(){
+        if(messagesHistory.size()>=1){
+            return messagesHistory.stream().toList();
+        }else{
+            return List.of();
+        }
+    }
+
     /**
      * Processes a message, adding it to the queue to be inserted and broadcasting it to websocket clients
      * @param message The message to process
      */
     public void processMessage(Message message){
-        insertQueue.add(message.generateDocument());
-
         if(insertQueue.size()>10){
             insertMessages();
         }
+
+        insertQueue.add(message.generateDocument());
+
+        messagesHistory.add(message);
+
+        Main.LOGGER.info(new Gson().toJson(message));
 
     }
 
